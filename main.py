ac4db105b405d3b7c629114eb05cad54e2400954
Quynh Nguyen <nhuquynh521@gmail.com>
2023-11-14 10:28:28 -0800

    finishes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ data = {
 
 }
 
-##
+#
 with open('data.json','w') as json_file:
 
     json.dump(data,json_file,indent=4)
@@ -25,3 +25,17 @@ print('Data has been written to data.json')
 with open('data.json','r') as json_file:
 
     loaded_data = json.load(json_file)
+
+print("Successfully able to read data.json")
+print(loaded_data)
+
+#modifying loaded data
+loaded_data['age'] = 34
+loaded_data['interests'].append('Cats')
+
+#
+with open('data.json','w') as json_file:
+
+    json.dump(loaded_data, json_file, indent=4)
+
+print('Modified data written to data.json')
