a2cbcdf2d0df907f18b06c1a5570ec1bbfd4cd89
Quynh Nguyen <nhuquynh521@gmail.com>
2023-11-14 23:19:47 -0800

    add comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #import the class library
 import json
 
-#create the data dictionary
 
+#create the data dictionary
 data = {
 
     'name': 'Quynh Nguyen',
@@ -13,7 +13,7 @@ data = {
 
 }
 
-#
+#write data into a json file
 with open('data.json','w') as json_file:
 
     json.dump(data,json_file,indent=4)
@@ -21,9 +21,9 @@ with open('data.json','w') as json_file:
 print('Data has been written to data.json')
 
 
-#
+#reading from json file
 with open('data.json','r') as json_file:
-
+#loading the data from json file
     loaded_data = json.load(json_file)
 
 print("Successfully able to read data.json")
@@ -31,9 +31,9 @@ print(loaded_data)
 
 #modifying loaded data
 loaded_data['age'] = 34
-loaded_data['interests'].append('Cats')
+loaded_data['interests'].append('Chocolate')
 
-#
+#writing modified data to the json file
 with open('data.json','w') as json_file:
 
     json.dump(loaded_data, json_file, indent=4)
